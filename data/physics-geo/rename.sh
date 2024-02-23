c870c1b2a40e603fb688c2ef4c0b5af1c5de370e
Sadamori Kojaku <skojaku@iu.edu>
2024-02-23 09:27:43 -0500

    physics

---
+++ data/physics-geo/rename.sh
@@ -0,0 +1,12 @@
+mv Screenshot\ 2024-02-18\ at\ 7.56.37 AM.png 01.png
+mv Screenshot\ 2024-02-18\ at\ 7.56.42 AM.png 02.png
+mv Screenshot\ 2024-02-18\ at\ 7.56.49 AM.png 03.png
+mv Screenshot\ 2024-02-18\ at\ 7.56.55 AM.png 04.png
+mv Screenshot\ 2024-02-18\ at\ 7.57.01 AM.png 05.png
+mv Screenshot\ 2024-02-18\ at\ 7.57.09 AM.png 06.png
+mv Screenshot\ 2024-02-18\ at\ 7.57.14 AM.png 07.png
+mv Screenshot\ 2024-02-18\ at\ 7.57.19 AM.png 08.png
+mv Screenshot\ 2024-02-18\ at\ 7.57.24 AM.png 09.png
+mv Screenshot\ 2024-02-18\ at\ 7.57.30 AM.png 10.png
+mv Screenshot\ 2024-02-18\ at\ 7.57.36 AM.png 11.png
+
